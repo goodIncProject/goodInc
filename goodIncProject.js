f6723285a3251a2f8c46c6ec1a0b028c6ac7bae3
mickeypugh <mickey.pugh@wayland.wbu.edu>
2020-09-15 14:38:03 -0500

    Added commands to JS file.

--- goodIncProject.js
+++ goodIncProject.js
@@ -1,5 +1,6 @@
-{
-var x = [1,2,3,4,5,6];
-myButton += x;
-let myButton = document.getElementById("Submit");
+function goodIncProject()   {
+
+<script>let myButton = document.getElementById("Submit").innerHTML = "Enter your name: "</script>
+alert("Hello! " + username);
+console.log(username);
 }
