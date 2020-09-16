600ec173bc35cb3acda68dc80c57fcc4fde0b2ca
Chris Robinson <christopher.j.robinson@wayland.wbu.edu>
2020-09-16 16:56:17 -0500

    Added: missing ">" after 'input' element. Fixed Error: "Element form is not closed" Fixed Warning: "Missing associated label" -line 30

--- goodIncProject.js
+++ goodIncProject.js
@@ -1,11 +1,18 @@
 function goodIncProject()   {
 
 }
+function helloUser() {
+    var a = document.getElementById("userInput").value;
+    alert("Hello, " + a);
+    document.getElementById("userInput").value = "";
+}
 
+/*
 
+What is this?
 
 <script>let myButton = document.getElementById("Submit").innerHTML = "Enter your name: "</script>
 <script>p>Enter Your Name Here: <input type ="text" name ="username"></p></script><br>
 alert("Hello! " + username);
-console.log(username);
-}
+console.log(username);*/
+
