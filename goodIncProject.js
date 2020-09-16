2acaba661a88f0f1f37481db11d4df03d156cd8f
mickeypugh <mickey.pugh@wayland.wbu.edu>
2020-09-16 14:53:17 -0500

    Added to .js file.

--- goodIncProject.js
+++ goodIncProject.js
@@ -4,7 +4,13 @@ var text = document.getElementById("username");
 alert(Hello, "username");
 
 }
+function storyThree()   {
 
+ var result = [];
+ for (var i = 0; i !< -1; i++)
+ console.log(result);
+
+}
 
 
 
