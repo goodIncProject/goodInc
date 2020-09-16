c956c531fc3b09404b2a9072d59d191c95dcbb2e
Chris Robinson <christopher.j.robinson@wayland.wbu.edu>
2020-09-16 17:48:43 -0500

    Added functions and html elements In goodIncProject.js: added: helloUser() Added: reverse() Added: toDocument() Added: alertWindow()
    In index.html:
    Fixed: input "username"
    Added: onclick="helloUser()"
    Added: JS Example of Editing HTML Document

--- goodIncProject.js
+++ goodIncProject.js
@@ -7,12 +7,38 @@ function helloUser() {
     document.getElementById("userInput").value = "";
 }
 
-/*
+function reverseText(){
+    alertWindow(reverse());
+}
 
-What is this?
+//reverses string
+function reverse(){
+    var userInput = prompt("A name / phrase / sentence", "");
+    var arrayInput = new Array(userInput.length);
+    var strReverse = "";
+    x = arrayInput.length;
+    arrayInput = userInput.split("");
+    for (i=0; i < x; i++){
+        strReverse += arrayInput.pop();
+    }
+    return strReverse;
+}
 
-<script>let myButton = document.getElementById("Submit").innerHTML = "Enter your name: "</script>
-<script>p>Enter Your Name Here: <input type ="text" name ="username"></p></script><br>
-alert("Hello! " + username);
-console.log(username);*/
+function example(){
+    var a = prompt("Say something: ", "");
+    toDocument(a);
+}
+
+//used to insert text into html document
+function toDocument(s){
+    //example
+    document.getElementById("example").innerHTML = s;
+}
+
+//redundant but allows more encapsulation of the functions.
+function alertWindow(s){
+    alert(s);
+}
+
+
 
